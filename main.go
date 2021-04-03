4fa2e25af38327c826801b165aa76fd3f67d3af2
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-03 11:14:59 -0700

    Add router configuration

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"os"
@@ -11,22 +12,52 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type GenericResponse struct {
-	Message string `json:"message"`
+type DiscordNotificationInfo struct {
+	UserID uint64 `json:"uid"`
+	Body   string `json:"body"`
+}
+
+type EmailNotificationInfo struct {
+	Address string `json:"address"`
+	Body    string `json:"body"`
 }
 
 func main() {
 	port := flag.Uint64("p", 7584, "service binding port")
 	flag.Parse()
 
-	_, err := discord.New(os.Getenv("UNIVERSALIS_ALERTS_DISCORD_TOKEN"))
+	// Instantiate services
+	discordClient, err := discord.New(os.Getenv("UNIVERSALIS_ALERTS_DISCORD_TOKEN"))
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	_ = email.New("", os.Getenv("UNIVERSALIS_MAILGUN_KEY"))
+	emailClient := email.New("", os.Getenv("UNIVERSALIS_MAILGUN_KEY"))
 
+	// Configure router
 	app := fiber.New()
+
+	discordRouter := app.Group("/discord")
+	discordRouter.Post("/send", func(ctx *fiber.Ctx) error {
+		dni := &DiscordNotificationInfo{}
+		if err := json.Unmarshal(ctx.Body(), dni); err != nil {
+			log.Println(err)
+			return err
+		}
+
+		return discordClient.SendNotification(dni.UserID, dni.Body)
+	})
+
+	emailRouter := app.Group("/email")
+	emailRouter.Post("/send", func(ctx *fiber.Ctx) error {
+		eni := &EmailNotificationInfo{}
+		if err := json.Unmarshal(ctx.Body(), eni); err != nil {
+			log.Println(err)
+			return err
+		}
+
+		return emailClient.SendNotification(eni.Address, eni.Body)
+	})
 
 	app.Listen(":" + strconv.FormatUint(*port, 10))
 }
