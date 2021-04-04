06ab5bb76356ea7396c3733980f4ec3c87a2576f
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-04 09:15:26 -0700

    Pull email contents out into a template The file email_template.html is embedded into the executable.

--- service/email/email_service.go
+++ service/email/email_service.go
@@ -1,7 +1,10 @@
 package email
 
 import (
+	"bytes"
 	"context"
+	_ "embed"
+	"text/template"
 	"time"
 
 	"github.com/Universalis-FFXIV/alerts/model"
@@ -9,28 +12,42 @@ import (
 	"github.com/mailgun/mailgun-go/v4"
 )
 
+//go:embed email_template.html
+var emailTemplate string
+
 type emailService struct {
 	client *mailgun.MailgunImpl
+	et     *template.Template
 }
 
 // New creates a new Mailgun-backed NotificationService.
-func New(domain string, key string) common.NotificationService {
+func New(domain string, key string) (common.NotificationService, error) {
 	client := mailgun.NewMailgun(domain, key)
-	email := &emailService{client: client}
-	return email
+	et, err := template.New("universalis_email_template").Parse(emailTemplate)
+	if err != nil {
+		return nil, err
+	}
+
+	email := &emailService{client: client, et: et}
+
+	return email, nil
 }
 
 func (e *emailService) SendNotification(address string, notification *model.Notification) error {
 	sender := "notifications@universalis.app"
 	subject := "Alert triggered for " + notification.ItemName
 
-	body := notification.Body + "\n\n<p>Your alert for " + notification.ItemName + " has been triggered.\nYou can view the item page on Universalis by clicking <a href=\"" + notification.PageURL + "\">this link</a>.</p>"
+	var body bytes.Buffer
+	err := e.et.Execute(&body, notification)
+	if err != nil {
+		return err
+	}
 
-	message := e.client.NewMessage(sender, subject, body, address)
+	message := e.client.NewMessage(sender, subject, body.String(), address)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
-	_, _, err := e.client.Send(ctx, message)
+	_, _, err = e.client.Send(ctx, message)
 
 	return err
 }
